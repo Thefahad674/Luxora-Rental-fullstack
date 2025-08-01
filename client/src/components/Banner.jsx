f001460b4990aa85d87439ed5932f25c811bf0d4
Thefahad674 <fahadf30511@gmail.com>
2025-08-01 21:43:00 +0530

    commit

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { assets } from "../assets/assets";
 import { motion } from "motion/react";
+import { UseAppContext } from "../context/AppContext";
 
 const Banner = () => {
+  const { navigate } = UseAppContext();
   return (
     <motion.div
       initial={{ opacity: 0, y: 50 }}
@@ -24,6 +26,7 @@ const Banner = () => {
           whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.95 }}
           className="px-6 py-2 bg-white hover:bg-slate-100 transition-all text-primary rounded-lg text-sm mt-4  cursor-pointer"
+          onClick={() => navigate("/owner/add-car")}
         >
           List your car
         </motion.button>
