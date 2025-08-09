6bea12a5271d179485bda5fa68fbd079bea38f13
Thefahad674 <fahadf30511@gmail.com>
2025-08-09 23:42:36 +0530

    initial commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,10 +4,12 @@ import FeaturedSection from '../components/FeaturedSection'
 import Banner from '../components/Banner'
 import Testimonial from '../components/Testimonial'
 import NewsLetter from '../components/NewsLetter'
+ 
 
 const Home = () => {
   return (
     <div>
+      
         <Hero />
         <FeaturedSection />
         <Banner />
